0838086969c23007542a43c34b54840ed40fd049
seungjun.lee <sigurnna@gmail.com>
2018-04-18 17:29:35 +0900

    - Change project directory and name

---
+++ UIPullToRefreshTableView/UIPullToRefreshTableView.h
@@ -0,0 +1,19 @@
+//
+//  UIPullToRefreshTableView.h
+//  UIPullToRefreshTableView
+//
+//  Created by 이승준 on 2018. 4. 18..
+//  Copyright © 2018년 seungjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIPullToRefreshTableView.
+FOUNDATION_EXPORT double UIPullToRefreshTableViewVersionNumber;
+
+//! Project version string for UIPullToRefreshTableView.
+FOUNDATION_EXPORT const unsigned char UIPullToRefreshTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIPullToRefreshTableView/PublicHeader.h>
+
+
--- UIPullToRefreshTableView/UIPullToRefreshTableView.swift
+++ UIPullToRefreshTableView/UIPullToRefreshTableView.swift
@@ -1,6 +1,6 @@
 //
 //  UIPullToRefreshTableView.swift
-//  PullToRefresh
+//  UIPullToRefreshTableView
 //
 //  Created by 이승준 on 2018. 4. 18..
 //  Copyright © 2018년 seungjun. All rights reserved.
