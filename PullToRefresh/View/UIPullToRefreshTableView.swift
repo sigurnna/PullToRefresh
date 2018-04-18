bc973336c848631f4df3126ba694047fb7413f4e
seungjun.lee <sigurnna@gmail.com>
2018-04-18 15:00:26 +0900

    - Loading Completion 감지를 위한 테스트 추가, 로딩이 종료된 이후에도 rotate anim이 적용되던 문제 수정.

--- PullToRefresh/View/UIPullToRefreshTableView.swift
+++ PullToRefresh/View/UIPullToRefreshTableView.swift
@@ -11,12 +11,17 @@ import UIKit
 class UIPullToRefreshTableView: UITableView {
     
     let refreshHoldHeight: CGFloat = 60
-    let refreshTriggerHeight: CGFloat = 75
+    let refreshTriggerHeight: CGFloat = 60
     
+    // Accessible Properties
     var loadingHandler: (() -> Void)?
+    var bgColor = UIColor.lightGray
+    var loadingColor = UIColor.gray
+    var spinningColor = UIColor.red
     
     private var progress: CGFloat = 0
     private var isLoading = false
+    private var isTriggered = false
     
     private lazy var refreshView: UIView = {
         return initRefreshView()
@@ -46,7 +51,12 @@ class UIPullToRefreshTableView: UITableView {
     func loadingComplete() {
         isLoading = false
         isScrollEnabled = true
-        contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        
+        shapeLayer.removeAnimation(forKey: "rotateAnim")
+        
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            self?.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        }
     }
     
     // MARK: - KVO
@@ -58,14 +68,17 @@ class UIPullToRefreshTableView: UITableView {
         
         let progress = abs(contentOffset.y) / refreshTriggerHeight
         
-        // TODO
-        // progress가 1이되면 rotating 되도록 수정을 해보도록 하자.
-        
         if self.isDragging {
-            updateRefreshProgress(progress)
+            if progress < 1.0 {
+                updateRefreshProgress(progress)
+            } else {
+                isTriggered = true
+                shapeLayer.strokeColor = spinningColor.cgColor
+            }
         } else {
-            if !isLoading && progress >= 1.0 {
+            if !isLoading && isTriggered {
                 isLoading = true
+                isTriggered = false
                 isScrollEnabled = false
                 
                 UIView.animate(withDuration: 0.3, animations: { [weak self] in
@@ -73,8 +86,10 @@ class UIPullToRefreshTableView: UITableView {
                         weakSelf.contentInset = UIEdgeInsetsMake(weakSelf.contentInset.top + weakSelf.refreshHoldHeight, 0, 0, 0)
                     }
                 }, completion: { [weak self] completion in
-                    self?.spinningRefreshIndicator()
-                    self?.loadingHandler?()
+                    if let weakSelf = self {
+                        weakSelf.spinningRefreshIndicator()
+                        weakSelf.loadingHandler?()
+                    }
                 })
             }
         }
@@ -85,7 +100,7 @@ class UIPullToRefreshTableView: UITableView {
     private func initRefreshView() -> UIView {
         let view = UIView()
         view.frame = CGRect(x: 0, y: -refreshHoldHeight, width: self.frame.size.width, height: refreshHoldHeight)
-        view.backgroundColor = UIColor.lightGray
+        view.backgroundColor = bgColor
         view.layer.addSublayer(shapeLayer)
 
         return view
@@ -93,14 +108,14 @@ class UIPullToRefreshTableView: UITableView {
     
     private func initShapeLayer() -> CAShapeLayer {
         let radius: CGFloat = 15.0
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: 0, endAngle: CGFloat(Double.pi * 1.9), clockwise: true)
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: 0, endAngle: CGFloat(Double.pi * 2.0), clockwise: true)
         
         let layer = CAShapeLayer()
         layer.frame = CGRect(x: (self.frame.width / 2) - radius, y: (refreshHoldHeight / 2) - radius, width: radius * 2, height: radius * 2)
         layer.path = circlePath.cgPath
-        layer.fillColor = UIColor.lightGray.cgColor
+        layer.fillColor = bgColor.cgColor
         layer.lineWidth = 3
-        layer.strokeColor = UIColor.red.cgColor
+        layer.strokeColor = loadingColor.cgColor
         layer.strokeStart = 0.0
         layer.strokeEnd = 0.0
         
@@ -118,20 +133,24 @@ class UIPullToRefreshTableView: UITableView {
         
         progress = newProgress
         
-        shapeLayer.removeAllAnimations()
+        shapeLayer.strokeColor = UIColor.gray.cgColor
+        shapeLayer.removeAnimation(forKey: "layerAnim")
         shapeLayer.add(layerAnim, forKey: "layerAnim")
     }
     
     private func spinningRefreshIndicator() {
-        let rotateAnim = CABasicAnimation(keyPath: "transform.rotation.z")
-        rotateAnim.fromValue = 0
-        rotateAnim.toValue = Double.pi * 2.0
-        rotateAnim.duration = 1.0
-        rotateAnim.isCumulative = true
-        rotateAnim.repeatCount = Float.infinity
-        //rotateAnim.timingFunction
-        
-        //shapeLayer.removeAllAnimations()
-        shapeLayer.add(rotateAnim, forKey: "rotateAnim")
+        if shapeLayer.animation(forKey: "rotateAnim") == nil {
+            let radius: CGFloat = 15.0
+            let circlePath = UIBezierPath(arcCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: 0, endAngle: CGFloat(Double.pi * 1.9), clockwise: true)
+            
+            let rotateAnim = CABasicAnimation(keyPath: "transform.rotation.z")
+            rotateAnim.fromValue = 0
+            rotateAnim.toValue = Double.pi * 2.0
+            rotateAnim.duration = 1.0
+            rotateAnim.repeatCount = Float.infinity
+            
+            shapeLayer.path = circlePath.cgPath
+            shapeLayer.add(rotateAnim, forKey: "rotateAnim")
+        }
     }
 }
